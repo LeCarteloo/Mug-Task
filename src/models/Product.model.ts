a96f44350ef89eb127f684b2fa4ea06652112ed1
Filip Papiernik <filippapiernik1999@gmail.com>
2022-11-05 13:35:32 +0100

    Created Product interface and model

---
+++ src/models/Product.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface IProduct {
+  name: string;
+  price: number;
+  updateDate: Date;
+}
+
+export interface IProductModel extends IProduct, Document {}
+
+const ProductSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  updateDate: {
+    type: Date,
+    required: true,
+    default: new Date(),
+  },
+});
+
+export default mongoose.model<IProductModel>('Product', ProductSchema);
